0344055e5442b35553b75951c09a54271f4942dd
Maybe <jiun0623@naver.com>
2015-08-16 11:44:46 +0900

    Step 2. 2.

--- TETRIS/TETRIS/main.c
+++ TETRIS/TETRIS/main.c
@@ -1,5 +1,7 @@
 ﻿#include "main.h"
 
+char matrix[12][23];
+
 int main()
 {
 	system("mode con:cols=35 lines=25");
@@ -26,12 +28,4 @@ int main()
 	printf("■□□□□□□□□□□■□□□□■\n");
 	printf("■□□□□□□□□□□■□□□□■\n");
 	printf("■■■■■■■■■■■■■■■■■\n");
-
-	COORD cur = { 1,2 };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
-	printf("☆");
-	cur.X = 3;
-	cur.Y = 10;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cur);
-	printf("☆");
 }
