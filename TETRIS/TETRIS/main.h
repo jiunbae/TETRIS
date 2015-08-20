37d8f25811c926dfe433c6c0cc121e4eba074ffa
Maybe <jiun0623@naver.com>
2015-08-20 17:02:05 +0900

    repair main.h

--- TETRIS/TETRIS/main.h
+++ TETRIS/TETRIS/main.h
@@ -5,7 +5,7 @@
 //stdio.h를 사용합니다.
 	//이 헤더의 printf함수가 사용되었습니다.
 
-#ifdef WIN32
+#ifdef _WIN32 || _WIN64
 //이 전처리 구문절은 WINDOWS에서만 실행됩니다.
 	#include <Windows.h>
 
@@ -13,5 +13,9 @@
 
 	#pragma warning ( disable : 4996)
 	//이 전처리 구문은 C4996에러를 무시합니다.
+
+#else
+
+	#include <unistd.h>
 
 #endif
